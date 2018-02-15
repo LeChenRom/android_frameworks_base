2417bcf9ddb4eb0847547088f048ab0bc574dcf1
xyyx <xyyx@mail.ru>
2018-02-15 13:20:21 +0800

    BatteryPercent: Allow disabling percent inside circle * increase percent's size in circle * show percent in quick sb header when circle without inside percent * fix inside percent in portrait and percent when charging. Thanks to ezio84 for suggestions
    Change-Id: I9e9451aec5b63bc4f7c5dea27fe66ef47d3b3b5e

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -88,7 +88,7 @@ public class BatteryMeterView extends LinearLayout implements
     private final int mFrameColor;
 
     private int mStyle = BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT;
-    private boolean mShowPercentText ;
+    private int mShowPercentText;
     private boolean mCharging;
 
     private final int mEndPadding;
@@ -161,10 +161,14 @@ public class BatteryMeterView extends LinearLayout implements
     public void setIsQuickSbHeaderOrKeyguard(boolean qs) {
         mQsHeaderOrKeyguard = qs;
     }
+
      private boolean forcePercentageQsHeader() {
-        return mQsHeaderOrKeyguard && (mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT
+        return mQsHeaderOrKeyguard
+                && ((mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT && mShowPercentText == 0)
+                || (mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_LANDSCAPE && mShowPercentText == 0)
                 || mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN
-                || mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT);
+                || mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT
+                || (isCircleBattery() && mShowPercentText == 0));
     }
 
     /**
@@ -234,7 +238,9 @@ public class BatteryMeterView extends LinearLayout implements
     @Override
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
 
-        if (isCircleBattery()) {
+        if (isCircleBattery()
+            || mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT
+            || mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_LANDSCAPE) {
             mForceShowPercent = pluggedIn;
             updateShowPercent();
         }
@@ -277,7 +283,7 @@ public class BatteryMeterView extends LinearLayout implements
     private void updateShowPercent() {
         final boolean showing = mBatteryPercentView != null;
         if (forcePercentageQsHeader()
-                || (mStyle != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN && (mShowPercentText || mForceShowPercent))) {
+                || (mStyle != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN && ((mShowPercentText == 1) || mForceShowPercent))) {
             if (!showing) {
                 mBatteryPercentView = loadPercentView();
                 if (mTextColor != 0) mBatteryPercentView.setTextColor(mTextColor);
@@ -297,7 +303,8 @@ public class BatteryMeterView extends LinearLayout implements
             mBatteryPercentView.setPaddingRelative(0, 0,
                     mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? mEndPaddingNoIcon : mEndPadding, 0);
         }
-        mDrawable.showPercentInsideCircle(!mShowPercentText);
+        mDrawable.showPercentInsideCircle(mShowPercentText == 2);
+        mDrawable.setShowPercent(mShowPercentText == 2);
     }
 
     @Override
@@ -408,7 +415,9 @@ public class BatteryMeterView extends LinearLayout implements
         }
 
         if (forcePercentageQsHeader()
-                || style == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT || (isCircleBattery() && mCharging)) {
+                || style == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT
+                || ((isCircleBattery() || style == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT
+                || style == BatteryMeterDrawableBase.BATTERY_STYLE_LANDSCAPE) && mCharging)) {
             mForceShowPercent = true;
         } else {
             mForceShowPercent = false;
@@ -419,7 +428,7 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     public void updateSettings(boolean fromObserver) {
-        mShowPercentText = 0 != Settings.System.getIntForUser(mContext.getContentResolver(),
+        mShowPercentText = Settings.System.getIntForUser(mContext.getContentResolver(),
                 SHOW_BATTERY_PERCENT, 1, mUser);
         mStyle = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 STATUS_BAR_BATTERY_STYLE, BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT, mUser);
